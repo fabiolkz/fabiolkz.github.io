cc5a6bcea731d273d8cef7b7932dacd4d2c6e676
fabiolkz <fabiodosantoslk@gmail.com>
2023-01-26 22:25:34 -0300

    Melhorando ainda mais ..

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 const title = document.querySelector('#title');
 const subTitle = document.querySelector('#sub-title');
 
-// console.log(subTitle.textContent.length);
-
-
-const typeWriter = (elemento) => {
-    const textoArray = elemento.innerHTML.split('');
-    textoArray.forEach((letra, i) => {
-        elemento.innerHTML = '';
-        setTimeout(() => {
-            elemento.innerHTML += letra;
-        }, 100 * i) 
-    })
-}
-
 const typeWriterFaster = (elemento) => {
     const textoArray = elemento.innerHTML.split('');
     textoArray.forEach((letra, i) => {
@@ -24,15 +11,9 @@ const typeWriterFaster = (elemento) => {
     })
 }
 
-// typeWriter(title, setTimeout(() => {
-//     subTitle.style.opacity = '100%';
-//     typeWriterFaster(subTitle);
-// }, 1200));
-
 typeWriterFaster(subTitle)
 
 // // FAZENDO EFEITO DE APARECER 
-
 
 
 const aparecerSlowly = () => {
@@ -54,40 +35,3 @@ const aparecerSlowly = () => {
 }
 
 aparecerSlowly();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// typeWriterTitle()
-
-// const typeComplete = () => {
-
-//     typeWriterTitle(title);
-
-//     setTimeout(() => {
-//         subTitle.style.opacity = '100%';
-//         typeWriterSubTitle(subTitle);
-//     }, 12 * 150)
-// }
-
-// typeComplete();
